b31073260e107a19d6b792a967adf4ccc6b0c992
SANTOS BERNUI <santosbernuy20@gmail.com>
2022-06-30 23:02:47 -0500

    capa entidad modelo

--- CapaEntidad/EntModelo.cs
+++ CapaEntidad/EntModelo.cs
@@ -8,5 +8,13 @@ namespace CapaEntidad
 {
     public class EntModelo
     {
+        public string CodModelo { get; set; }
+        public string DesModelo { get; set; }
+        public string CodTipoMadera { get; set; }
+        public string ColorModelo { get; set; }
+        public float PrecioVentaPU { get; set; }
+        public float PrecioVentaPM { get; set; }
+        public string CodTipoMueble { get; set; }
+        public Boolean EstadoModelo { get; set; }
     }
 }
