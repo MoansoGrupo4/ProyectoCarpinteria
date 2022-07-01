a3f943b23f5b9f40c172f305112803a83f3a8156
yisus0607 <santosbernuy20@gmail.com>
2022-07-01 14:51:29 -0500

    logica funcion

---
+++ CapaLogica/LogFuncion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaAccesoDatos;
+using CapaEntidad;
+
+namespace CapaLogica
+{
+    public class LogFuncion
+    {
+        #region sigleton
+
+        private static readonly LogFuncion _instancia = new LogFuncion();
+        //privado para evitar la instanciación directa
+        public static LogFuncion Instancia
+        {
+            get
+            {
+                return LogFuncion._instancia;
+            }
+        }
+        #endregion singleton
+
+        public List<EntFuncion> ListarFuncion()
+        {
+            return DatFuncion.Instancia.ListarFuncion();
+        }
+    }
+}
