eb5f59d536d383c15419803755bc278e2e8a31e5
Elvis Wilfredo Paz Carrillo <N00197685@upn.pe>
2022-07-04 03:41:19 -0500

    Revert "Avance 1"
    This reverts commit 1528e1d8297845f083157f03dc96e3231a5e0d3c.

--- FormularioCarpinteria/FormTransaccionOP.cs
+++ FormularioCarpinteria/FormTransaccionOP.cs
@@ -7,38 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CapaEntidad;
-using CapaLogica;
+
 namespace FormularioCarpinteria
 {
     public partial class FormTransaccionOP : Form
     {
-        EntOP OP = new EntOP();
-        private List<EntOP> listarOP = new List<EntOP>();
         public FormTransaccionOP()
         {
             InitializeComponent();
-        
         }
+
         private void lblCliente_Click(object sender, EventArgs e)
         {
-            
+
         }
+
         private void Quitar_Click(object sender, EventArgs e)
         {
 
         }
+
         private void txtBuscar_TextChanged(object sender, EventArgs e)
         {
             //oli
-        }
-        private void FormTransaccionOP_Load(object sender, EventArgs e)
-        {
-
-        }
-        private void btnBuscar_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
