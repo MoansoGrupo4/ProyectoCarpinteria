4926711d6909b494c59651316f2c66b4a7af080e
Elvis Wilfredo Paz Carrillo <N00197685@upn.pe>
2022-07-04 20:46:52 -0500

    PJose

---
+++ CapaAccesoDatos/DatPedido.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data;
+using System.Data.SqlClient;
+namespace CapaAccesoDatos
+{
+    public class DatPedido:Conexion
+    {
+        private static readonly DatPedido _instancia = new DatPedido();
+        public static new DatPedido Instancia
+        {
+            get
+            {
+                return DatPedido._instancia;
+            }
+        }
+      
+        
+        public EntCliente BuscarIdPedido(string CodPedido)
+        {
+            SqlCommand cmd = null;
+            EntPedido Pedido = new EntPedido();
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spBuscarPedido", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CodPedido", CodPedido);
+                cn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    Pedido.CodPedido = dr["CodPedido"].ToString();
+                    Pedido.Descripcion = dr["descripcion"].ToString();
+                    Pedido.Cantidad = Convert.ToInt32(dr["cantidad"]);
+                    Pedido.Precio = Convert.ToDecimal(dr["precio"]);
+                    Pedido.Codigo.Codigo = Convert.ToInt32(dr["CodCliente1"]);
+                    Pedido.CodModelo.CodModelo = dr["CodModelo1"].ToString();
+                    Pedido.Precio = Convert.ToDecimal(dr["precio"]);
+
+                    Cli.Codigo = Convert.ToInt32(dr["CodCLiente"]);
+                    Cli.Cliente = dr["NombreCliente"].ToString();
+                    Cli.Razon_Social = dr["RazonSocialCliente"].ToString();
+                    Cli.Direccion = dr["dirCliente"].ToString();
+                    Cli.Registro = Convert.ToDateTime(dr["FechaRegCliente"]);
+                    Cli.Telefono = Convert.ToInt32(dr["telefono"]);
+                    Cli.Estado = Convert.ToBoolean(dr["EstadoCliente"]);
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            finally { cmd.Connection.Close(); }
+            return Cli;
+        }
+}
--- CapaEntidad/EntPedido.cs
+++ CapaEntidad/EntPedido.cs
@@ -8,6 +8,16 @@ namespace CapaEntidad
 {
     public class EntPedido
     { 
-      public string  CodPedido { get; set; }
-}
+        public string  CodPedido { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+
+        public EntCliente Codigo { get; set; }
+
+        public EntModelo CodModelo { get; set; }
+
+        public decimal total { get; set; }
+        public List<EntDetPedido> DetPedidos { get; set; }
+    }
 }
