5e35659bbd5976f65a754a5ec8da307ac4d6eef0
yisus0607 <santosbernuy20@gmail.com>
2022-07-04 23:22:20 -0500

    btnasignar

--- FormularioCarpinteria/FormAsignarEmpleados.Designer.cs
+++ FormularioCarpinteria/FormAsignarEmpleados.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBoxEmpleado = new System.Windows.Forms.GroupBox();
+            this.groupBoxOrdendeproduccion = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // groupBoxEmpleado
+            // 
+            this.groupBoxEmpleado.ForeColor = System.Drawing.Color.White;
+            this.groupBoxEmpleado.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxEmpleado.Name = "groupBoxEmpleado";
+            this.groupBoxEmpleado.Size = new System.Drawing.Size(943, 180);
+            this.groupBoxEmpleado.TabIndex = 0;
+            this.groupBoxEmpleado.TabStop = false;
+            this.groupBoxEmpleado.Text = "Empleado:";
+            // 
+            // groupBoxOrdendeproduccion
+            // 
+            this.groupBoxOrdendeproduccion.ForeColor = System.Drawing.Color.White;
+            this.groupBoxOrdendeproduccion.Location = new System.Drawing.Point(12, 209);
+            this.groupBoxOrdendeproduccion.Name = "groupBoxOrdendeproduccion";
+            this.groupBoxOrdendeproduccion.Size = new System.Drawing.Size(943, 203);
+            this.groupBoxOrdendeproduccion.TabIndex = 1;
+            this.groupBoxOrdendeproduccion.TabStop = false;
+            this.groupBoxOrdendeproduccion.Text = "Orden de Produccion:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1034, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1034, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1034, 148);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1034, 209);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(142, 38);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // FormAsignarEmpleados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 558);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(10)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1227, 558);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBoxOrdendeproduccion);
+            this.Controls.Add(this.groupBoxEmpleado);
             this.Name = "FormAsignarEmpleados";
             this.Text = "FormAsignarEmpleados";
             this.ResumeLayout(false);
@@ -42,5 +111,12 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxEmpleado;
+        private System.Windows.Forms.GroupBox groupBoxOrdendeproduccion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
