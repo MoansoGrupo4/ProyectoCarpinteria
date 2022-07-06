fbd434fc2a910292b015e230eedbfda8bb6dd220
yisus0607 <santosbernuy20@gmail.com>
2022-07-06 12:35:42 -0500

    me da ansiedad..  ._.

--- FormularioCarpinteria/FormAsignarEmpleados.Designer.cs
+++ FormularioCarpinteria/FormAsignarEmpleados.Designer.cs
@@ -30,14 +30,24 @@
         {
             this.groupBoxEmpleado = new System.Windows.Forms.GroupBox();
             this.groupBoxOrdendeproduccion = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnQuitar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnBuscarEmpleado = new System.Windows.Forms.Button();
+            this.groupBoxEmpleado.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxEmpleado
             // 
+            this.groupBoxEmpleado.Controls.Add(this.btnBuscarEmpleado);
+            this.groupBoxEmpleado.Controls.Add(this.textBox2);
+            this.groupBoxEmpleado.Controls.Add(this.textBox1);
+            this.groupBoxEmpleado.Controls.Add(this.label2);
+            this.groupBoxEmpleado.Controls.Add(this.label1);
             this.groupBoxEmpleado.ForeColor = System.Drawing.Color.White;
             this.groupBoxEmpleado.Location = new System.Drawing.Point(12, 12);
             this.groupBoxEmpleado.Name = "groupBoxEmpleado";
@@ -56,41 +66,74 @@
             this.groupBoxOrdendeproduccion.TabStop = false;
             this.groupBoxOrdendeproduccion.Text = "Orden de Produccion:";
             // 
-            // button1
+            // btnAgregar
             // 
-            this.button1.Location = new System.Drawing.Point(1034, 38);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 38);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAgregar.Location = new System.Drawing.Point(1034, 38);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(142, 38);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnQuitar
             // 
-            this.button2.Location = new System.Drawing.Point(1034, 94);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(142, 38);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnQuitar.Location = new System.Drawing.Point(1034, 94);
+            this.btnQuitar.Name = "btnQuitar";
+            this.btnQuitar.Size = new System.Drawing.Size(142, 38);
+            this.btnQuitar.TabIndex = 3;
+            this.btnQuitar.Text = "QUITAR";
+            this.btnQuitar.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnSalir
             // 
-            this.button3.Location = new System.Drawing.Point(1034, 148);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(142, 38);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnSalir.Location = new System.Drawing.Point(1034, 148);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(142, 38);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // label1
             // 
-            this.button4.Location = new System.Drawing.Point(1034, 209);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(142, 38);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Empleado:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombres:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(391, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(563, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btnBuscarEmpleado
+            // 
+            this.btnBuscarEmpleado.ForeColor = System.Drawing.Color.Black;
+            this.btnBuscarEmpleado.Location = new System.Drawing.Point(571, 51);
+            this.btnBuscarEmpleado.Name = "btnBuscarEmpleado";
+            this.btnBuscarEmpleado.Size = new System.Drawing.Size(110, 28);
+            this.btnBuscarEmpleado.TabIndex = 4;
+            this.btnBuscarEmpleado.Text = "BUSCAR";
+            this.btnBuscarEmpleado.UseVisualStyleBackColor = true;
             // 
             // FormAsignarEmpleados
             // 
@@ -98,14 +141,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(10)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(1227, 558);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnQuitar);
+            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.groupBoxOrdendeproduccion);
             this.Controls.Add(this.groupBoxEmpleado);
             this.Name = "FormAsignarEmpleados";
             this.Text = "FormAsignarEmpleados";
+            this.groupBoxEmpleado.ResumeLayout(false);
+            this.groupBoxEmpleado.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -114,9 +158,13 @@
 
         private System.Windows.Forms.GroupBox groupBoxEmpleado;
         private System.Windows.Forms.GroupBox groupBoxOrdendeproduccion;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnQuitar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscarEmpleado;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
