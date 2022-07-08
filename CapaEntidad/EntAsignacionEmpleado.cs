7c7670305cfcdf9f539fcc5ab3d73942d210f51e
yisus0607 <santosbernuy20@gmail.com>
2022-07-07 22:35:00 -0500

    ent asig

--- CapaEntidad/EntAsignacionEmpleado.cs
+++ CapaEntidad/EntAsignacionEmpleado.cs
@@ -8,5 +8,14 @@ namespace CapaEntidad
 {
     public class EntAsignacionEmpleado
     {
+        public string IdGrupo { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public string IdEmpleado { get; set; }
+        public string IdOp { get; set; }
+        public int HorasTrabajo { get; set; }
+        public float PagoHora { get; set; }
+        public float PagoTotal { get; set; }
+
     }
 }
